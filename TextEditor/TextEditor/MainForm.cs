bfdba2b93db6b1513cd32480468af00955983902
msafiulin <msafiulin@deloitte.com>
2015-03-24 18:28:46 +0200

    IMainForm interface inplementation, add events.

--- TextEditor/TextEditor/MainForm.cs
+++ TextEditor/TextEditor/MainForm.cs
@@ -10,11 +10,65 @@ using System.Windows.Forms;
 
 namespace TextEditor
 {
-    public partial class MainForm : Form
+    public interface IMainForm
+    {
+        string FilePath { get; }
+        string Content { get; set; }
+        void SetSymbolCount(int count);
+        event EventHandler FileOpenClick;
+        event EventHandler FileSaveClick;
+        event EventHandler ContentChanged;
+    }
+
+    public partial class MainForm : Form, IMainForm
     {
         public MainForm()
         {
             InitializeComponent();
+            butOpenFile.Click += butOpenFile_Click;
+            butSaveFile.Click += butSave_Click;
+            fldContent.TextChanged += fldContent_TextChanged;
         }
+
+        #region events
+        private void butOpenFile_Click(object sender, EventArgs e)
+        {
+            if (FileOpenClick != null) FileOpenClick(this, EventArgs.Empty);
+        }
+
+        private void butSave_Click(object sender, EventArgs e)
+        {
+            if (FileSaveClick != null) FileSaveClick(this, EventArgs.Empty);
+        }
+
+        private void fldContent_TextChanged(object sender, EventArgs e)
+        {
+            if (ContentChanged != null) ContentChanged(this, EventArgs.Empty);
+        }
+        #endregion
+
+        #region IMainForm
+        public string FilePath
+        {
+            get { return fldFilePath.Text; }
+        }
+
+        public string Content
+        {
+            get { return fldContent.Text; }
+            set { fldContent.Text = value; }
+        }
+
+        public void SetSymbolCount(int count)
+        {
+            lblSymbolsCount.Text = count.ToString();
+        }
+
+        public event EventHandler FileOpenClick;
+        public event EventHandler FileSaveClick;
+        public event EventHandler ContentChanged;
+        #endregion
+
+        
     }
 }
