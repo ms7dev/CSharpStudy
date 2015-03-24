96e4a0b3426a8965be81123482aff68404c6d838
msafiulin <msafiulin@deloitte.com>
2015-03-24 18:00:48 +0200

    Install app. Add Main Form design.

---
+++ TextEditor/TextEditor.BL/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextEditor.BL
+{
+    public class FileManager
+    {
+        private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+
+        public bool IsExist(string filePath)
+        {
+            bool isExist = File.Exists(filePath);
+            return isExist;
+        }
+
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultEncoding);
+        }
+
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+
+        public void SaveContent(string content, string filePath)
+        {
+            SaveContent(content, filePath, _defaultEncoding);
+        }
+
+        public void SaveContent(string content, string filePath, Encoding encoding)
+        {
+            File.WriteAllText(filePath, content, encoding);
+        }
+
+        public int GetSymbolCount(string content)
+        {
+            int count = content.Length;
+            return count;
+        }
+    }
+}
